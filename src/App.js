8ececa00b7d431f58ebb9dbeaa5a1d3f36679390
Granit Dula <granitdula98@gmail.com>
2021-09-21 02:16:24 +0100

    feat(navbar): added a navbar to the page (#2)
    Added a navbar with my name as a logo and some links to sections that will be included in the main section of the page. The navbar is responsive to window width, where a burger menu appears if the width reaches a size of 45em. The burger menu can be clicked to bring a dropdown of all the links. The dropdown is automatically removed when the screen width is increased again, such that the links appear in the navbar again.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <h1>Granit's Website</h1>
+    <Navbar/>
   );
 }
 
