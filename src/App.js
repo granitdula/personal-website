04943d4cb3d3ca7bc8c14ec7b2a83f1f92876431
Granit Dula <granitdula98@gmail.com>
2021-09-25 19:52:52 +0100

    feat(page-structure): cleaned and re-structured App.js (#3)
    Deleted the default CSS in the App.css file and replaced it with the correct CSS for the newly structured divs in App.js.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <Navbar/>
+    <div className='main-container'>
+      <Navbar/>
+      <section className='content'>
+        <section className='home'></section>
+        <section className='about'></section>
+        <section className='timeline'></section>
+        <section className='projects'></section>
+        <section className='contact'></section>
+      </section>
+      <footer></footer>
+    </div>
   );
 }
 
