fb50b9135e32e66b5c23c335b724329e86cb889e
granitdula <granitdula98@gmail.com>
2021-09-25 15:32:59 +0100

    Added the page sections and skeleton styles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ function App() {
   return (
     <div className='main-container'>
       <Navbar/>
-      <section className='content'></section>
+      <section className='content'>
+        <section className='home'></section>
+        <section className='about'></section>
+        <section className='timeline'></section>
+        <section className='projects'></section>
+        <section className='contact'></section>
+      </section>
       <footer></footer>
     </div>
   );
