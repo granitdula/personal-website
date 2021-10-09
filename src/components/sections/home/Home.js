8ccdcd29eae30d6ebd38707d193cd68daf8b1eae
Granit Dula <granitdula98@gmail.com>
2021-10-09 17:06:30 +0100

    feat(home-section): added the home section to the page (#4)
    Added the home section which includes a diagonally coloured background, with a small summary text and profile image of me. The contents of the home section are also fully scalable under different window sizes.

---
+++ src/components/sections/home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Home.css';
+import profileImage from '../../../resources/images/me.jpg';
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <div className='home-background'>
+        <div className='background-layer-1'>
+          <h1>Hi, I'm Granit</h1>
+          <h3>Welcome to my portfolio page.</h3>
+        </div>
+        <div className='background-layer-2'></div>
+        <div className='background-layer-3'>
+          <img src={profileImage} alt='#'></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home
