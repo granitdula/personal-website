bda33f7b05d8b3a02d463fc2e125ff8f7d515c46
granitdula <granitdula98@gmail.com>
2021-10-17 22:47:06 +0100

    Added achievement content to flipcard

--- src/components/flipcard-contents/achievement-content/AchievementContent.js
+++ src/components/flipcard-contents/achievement-content/AchievementContent.js
@@ -4,7 +4,14 @@ import './AchievementContent.css';
 const AchievementContent = () => {
   return (
     <div className='achievement-content-container'>
-      
+      <ul>
+        <li>Achieved first class honours (78%) in MEng Computer Science at UCL.</li>
+        <li>Built full-working prototype apps in all hackathons I've attended.</li>
+        <li>
+          Won a competition amongst fellow interns on presenting a company for a
+          merge and acquisition. Presented aspects of NLP.
+        </li>
+      </ul>
     </div>
   );
 }
