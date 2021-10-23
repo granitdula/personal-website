875ee7d626fa5f1af5ff3b0f41facffb4547b2df
granitdula <granitdula98@gmail.com>
2021-10-23 02:13:07 +0100

    Added interest flipcard in about section

--- src/components/flipcard-contents/interest-content/InterestContent.js
+++ src/components/flipcard-contents/interest-content/InterestContent.js
@@ -4,7 +4,15 @@ import './InterestContent.css';
 const InterestContent = () => {
   return (
     <div className='interest-content-container'>
-      
+      <ul>
+        <li>Game Development</li>
+        <li>Web App Development</li>
+        <li>Machine Learning</li>
+        <li>Robotics/Microcontroller Programming</li>
+        <li>VR/AR Development</li>
+        <li>Computer Graphics</li>
+        <li>Image Processing</li>
+      </ul>
     </div>
   );
 }
