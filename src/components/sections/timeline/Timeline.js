65c7ff424a64346fc20bef7eebe89a09c509ef44
granitdula <granitdula98@gmail.com>
2021-10-31 21:03:14 +0000

    Added template for timeline component to App..js

---
+++ src/components/sections/timeline/Timeline.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Timeline.css';
+
+class Timeline extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Timeline
