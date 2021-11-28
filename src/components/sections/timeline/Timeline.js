e5167d1532399d17075af0ef49eee4fc5dbddf49
granitdula <granitdula98@gmail.com>
2021-11-28 19:23:25 +0000

    Added a descriptive subheading

--- src/components/sections/timeline/Timeline.js
+++ src/components/sections/timeline/Timeline.js
@@ -6,12 +6,14 @@ import timelineData from '../../../resources/component-data/timeline-data.json';
 class Timeline extends React.Component {
   constructor() {
     super();
+    
   }
 
   render() {
     return (
       <div className='timeline-container'>
         <h1>Timeline</h1>
+        <h3>A timeline of my experience of Computer Science during my education.</h3>
         <TimelineGraph timelineData={timelineData}/>
       </div>
     );
