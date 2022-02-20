1cfd3807d1877cf03ddb1a8ea29b895ae6c88cf9
granitdula <granitdula98@gmail.com>
2022-02-20 23:08:02 +0000

    Added skeleton for contact section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/sections/home/Home';
 import About from './components/sections/about/About';
 import Timeline from './components/sections/timeline/Timeline';
 import Projects from './components/sections/projects/Projects';
+import Contact from './components/sections/contact/Contact';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <section className='about'><About/></section>
         <section className='timeline'><Timeline/></section>
         <section className='projects'><Projects/></section>
-        <section className='contact'></section>
+        <section className='contact'><Contact/></section>
       </section>
       <footer></footer>
     </div>
