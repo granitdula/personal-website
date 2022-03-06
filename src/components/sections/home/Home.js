a1dd7965873f9652a4b276382074990f2326d8f1
Granit Dula <granitdula98@gmail.com>
2022-03-06 00:45:55 +0000

    Fixed scaling issues with home section (#12)
    Fixed scaling of the home section for all different resolutions up to 4k monitors. Also fixed the aspect ratio of my image to remove stretching effects.

--- src/components/sections/home/Home.js
+++ src/components/sections/home/Home.js
@@ -6,15 +6,14 @@ const Home = () => {
   return (
     <div className='home-container'>
       <div className='home-background'>
-        <div className='background-layer-1'>
-          <h1>Hi, I'm Granit</h1>
-          <h3>Welcome to my portfolio page.</h3>
-        </div>
+        <div className='background-layer-1'></div>
         <div className='background-layer-2'></div>
         <div className='background-layer-3'>
           <img src={profileImage} alt='#'></img>
         </div>
       </div>
+      <h1>Hi, I'm Granit</h1>
+      <h3>Welcome to my portfolio page.</h3>
     </div>
   );
 }
