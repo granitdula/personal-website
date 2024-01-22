7566f087afc30e80b17879323270f9c74f2c43e0
Granit Dula <granitdula98@gmail.com>
2024-01-22 19:35:55 +0000

    revert: remove contact link

--- src/components/sections/contact/Contact.js
+++ src/components/sections/contact/Contact.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Contact.css';
 import { ReactComponent as GithubIcon } from '../../../resources/images/section-images/github-icon.svg';
-import { ReactComponent as InstagramIcon } from '../../../resources/images/section-images/instagram-icon.svg';
 import { ReactComponent as LinkedinIcon } from '../../../resources/images/section-images/linkedin-icon.svg';
 import { ReactComponent as EmailIcon } from '../../../resources/images/section-images/email-icon.svg';
 
@@ -21,7 +20,6 @@ const Contact = () => {
           <h2>Social Media</h2>
           <div className='icon-container'>
             <a href='https://github.com/granitdula' target='_blank'><GithubIcon className='icon-image'/></a>
-            <a href='https://www.instagram.com/granitdula/' target='_blank'><InstagramIcon className='icon-image'/></a>
             <a href='https://www.linkedin.com/in/granit-dula-9bb02a176/' target='_blank'>
               <LinkedinIcon className='icon-image'/>
             </a>
